f08ed80f320ed34178f283d9bea74c8544b5c3f8
SuZIForD <chemistsuser@gmail.com>
2024-03-14 03:10:22 +0200

    Initial commit

---
+++ src/main/java/net/suziford/suzitest/SuziModClient.java
@@ -0,0 +1,10 @@
+package net.suziford.suzitest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SuziModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
